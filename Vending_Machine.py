96816c8046f551c239be03a679a0db23e01b76c0
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-12-15 20:44:33 +0400

    Update Vending_Machine.py

--- Vending_Machine.py
+++ Vending_Machine.py
@@ -1,7 +1,7 @@
-
-
-
-
+#Utility App
+'''Your task is to create a Vending Machine program using the Python programming language. 
+The program should demonstrate your knowledge of programming and make use of the techniques 
+introduced over the course of the module.'''
 
 def start():
 #Name of the Vending Machine
@@ -38,18 +38,87 @@ def start():
 |#|===========================|#|_________|##|
 |#|```````````````````````````|##############|
 |############################################|
-|#|||||||||||||||||||||||||||||####```````###|
-|#||||||||||||PUSH|||||||||||||####\|||||/###|
+|#|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|####|`````|###|
+|#|__________|PUSH|___________|####|\|||/|###|
 |############################################|
  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////////
- |________________________________|CR98|___|''')
+ |_________________________________________|''')
     print()
-    print('\t     Welcome to Moka Pot <3')
-    print('\t  A cafe in a vending machine!')
+    print('\t    Welcome to Moka Pot <3')
+    print('\t A cafe in a vending machine \n')
+    print('Everything you can buy at a cafe is now in a machine!')
 
 start()
 
 def main():
+#A list of drinks that are available
     drinks_available = [
-
+        {
+            'name':'Caffe Latte',
+            'price':20,
+            'code':'A1'
+        },
+        {
+            'name':'Frappe',
+            'price':23,
+            'code':'A2'
+        },
+        {
+            'name':'Cappuccino',
+            'price':20,
+            'code':'A3'
+        },
+        {
+            'name':'Double Espresso',
+            'price':23,
+            'code':'A4'
+        },
+        {
+            'name':'Macha Latte',
+            'price':15,
+            'code':'A5'
+        },
+        {
+            'name':'Strawberry & Cream',
+            'price':15,
+            'code':'A6'
+        }
     ]
+#A list of snacks that are available
+    snacks_available = [
+        {
+            'name':'Breakfast Sandwich',
+            'price':15,
+            'code':'B1'
+        },
+        {
+            'name':'Croissant',
+            'price':9,
+            'code':'B2'
+        },
+        {
+            'name':'Macarons (4 pieces)',
+            'price':15,
+            'code':'B3'
+        },
+        {
+            'name':'Cupcake',
+            'price':7,
+            'code':'B4'
+        }
+    ]
+
+    item = []
+
+    print('''\n\t      ))                 ((
+\t     |~~|  Drinks Menu  |~~|
+\t    C|__|               |__|]\n''')  
+    print('-----------------------------------------------------')
+    for i in drinks_available:
+        print(f"Drink: {i['name']} - Price: {i['price']} - Code: {i['code']}")
+        print()
+    print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
+            
+        
+
+main()
